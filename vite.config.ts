f13efc0b7a6c7079e77f86fdcb2c498f53f5c1ac
xKQAx <santiagocacuav@gmail.com>
2025-07-05 20:46:13 -0500

    Changes for render

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: true,
-    port: Number(process.env.PORT) || 4173,
-    allowedHosts: ['invitacion-mariana.onrender.com'],
-  },
+preview: {
+  host: true,
+  port: Number(process.env.PORT) || 4173,
+  allowedHosts: ['invitacion-mariana.onrender.com'],
+},
 })
